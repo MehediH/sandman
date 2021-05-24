c884baa302f487b5fb0047b02fdb72584b4b4335
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-05-24 19:43:48 +0100

    auth work

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,67 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+const options = {
+  providers: [
+    Providers.Spotify({
+      clientId: process.env.NEXT_PUBLIC_SPOTIFY_ID,
+      clientSecret: process.env.NEXT_PUBLIC_SPOTIFY_SECRET,
+      scope: [
+        'streaming',
+        'user-read-email',
+        'user-read-private',
+        'user-library-read',
+        'user-library-modify',
+        'user-read-playback-state',
+        'user-modify-playback-state',
+      ]
+    }),
+  ],
+  jwt: true,
+  callbacks: {
+    session: async (session, user) => {
+      if (session && user) {
+        session.user = user;
+
+      }
+      return Promise.resolve(session);
+
+    },
+    jwt: async (token, user, account, profile, isNewUser) => {
+      if (account) {
+        const now = new Date();
+        now.setHours(now.getHours() + 1);
+        token.access_token = account.accessToken;
+        token.refresh_token = account.refreshToken;
+        token.accessTokenExpires = now.getTime();
+        token.profile = profile;
+      }
+
+      if (Date.now() > token?.accessTokenExpires) {
+        token.accessToken = await generateNewToken(token.refreshToken);
+      }
+
+      return Promise.resolve(token);
+    }
+  }
+}
+
+
+const generateNewToken = async (refresh_token) => {
+  const newToken = await fetch('https://accounts.spotify.com/api/token', {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      'grant_type': "refresh_token",
+      'refresh_token': refresh_token,
+      'client_id': process.env.SPOTIFY_ID,
+      'client_secret': process.env.SPOTIFY_SECRET
+    }),
+  }).then(res => res.json());
+
+  return newToken.access_token;
+}
+
+export default (req, res) => NextAuth(req, res, options);
