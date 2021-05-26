0a36abaca017d5403e2bf1df0192856c0f2f3f49
Jack Burgess <19895932+jackthb@users.noreply.github.com>
2021-05-26 12:39:57 +0100

    get bearer tokens from session

--- pages/api/searchSpotify.js
+++ pages/api/searchSpotify.js
@@ -1,46 +1,51 @@
-export const searchSpotify = async ( q ) => {
-	try{
-        const songName = encodeURIComponent(q.split("by")[0]);
+import { getSession } from "next-auth/client";
 
-        let artistName = q.split("by")[1].substr(1);
+export const searchSpotify = async (q, access_token) => {
+  try {
+    const songName = encodeURIComponent(q.split("by")[0]);
+    console.log(access_token);
+    let artistName = q.split("by")[1].substr(1);
 
-        if(artistName.indexOf("(") !== -1){
-            artistName = artistName.substr(0, artistName.indexOf("(")) // need to filter out features like (ft. 2 Chainz)
-        }
+    if (artistName.indexOf("(") !== -1) {
+      artistName = artistName.substr(0, artistName.indexOf("(")); // need to filter out features like (ft. 2 Chainz)
+    }
 
-        artistName = encodeURIComponent(artistName);
+    artistName = encodeURIComponent(artistName);
 
-        const data = await fetch(
-            `https://api.spotify.com/v1/search?q=${songName + `%20artist:${artistName}`}&type=track`, 
-            {
-                headers: {
-                    'Authorization': `Bearer ${process.env.NEXT_PUBLIC_SPOTIFY_BEARER}`
-                }
-            }
-        ).then(res => res.json());
+    const data = await fetch(
+      `https://api.spotify.com/v1/search?q=${
+        songName + `%20artist:${artistName}`
+      }&type=track`,
+      {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
+      }
+    ).then((res) => res.json());
+    if (data.error) {
+      return { data: null, err: data.error.message };
+    }
 
-        if(data.error){
-            return { data: null, err: data.error.message }
-        }
-
-        return { data: data.tracks.items, err: null };
-	} catch(err){
-		return { data: null, err: err.message };
-	}
-}
+    return { data: data.tracks.items, err: null };
+  } catch (err) {
+    return { data: null, err: err.message };
+  }
+};
 
 // endpoint accepts query in the form "[songName] by [artistName]"
 // e.g. "Phoenix by A$AP Rocky"
 export default async function spotifySearchEndpoint(req, res) {
-	const { q } = req.query;
+  const { q } = req.query;
+  const session = await getSession({ req });
 
-    if(!q) return res.status(400).send("'q' parameter is missing for the request.");
+  if (!q)
+    return res.status(400).send("'q' parameter is missing for the request.");
 
-	const { data, err } = await searchSpotify(q);
+  const { data, err } = await searchSpotify(q, session.user.access_token);
 
-    if(err){
-        return res.status(400).send({ err });
-    }
+  if (err) {
+    return res.status(400).send({ err });
+  }
 
-	return res.status(200).send({ data });
+  return res.status(200).send({ data });
 }
